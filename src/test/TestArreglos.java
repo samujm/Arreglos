24ebaa3d03512445858290a3e6a1dfad7518068b
Samantha González <samujm76@gmail.com>
2022-08-14 17:40:21 -0500

    Definición de un arreglo

--- src/test/TestArreglos.java
+++ src/test/TestArreglos.java
@@ -3,5 +3,11 @@ package test;
 
 
 public class TestArreglos {
-    
+
+    public static void main(String[] args) {
+        //Definir un arreglo de tipo entero
+        //NOTA: Los arreglos no pueden crecer, no se pueden agregar elementos de manera dinamica, para colecciones dinamicas son listas, set o map
+        int edades[] = new int[3]; //tipo nombre[] = new(YA QUE ES UN TIPO OBJECT) tipo_dato[Cantidad de elementos];
+        System.out.println("edades = " + edades);//Imprime la dirección de memoria
+    }
 }
