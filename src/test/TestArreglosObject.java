6acc80c7fd92fba235e3d9efd3ecb8bb13ac0d2a
Samantha González <samujm76@gmail.com>
2022-08-14 18:27:13 -0500

    Iterar el arreglo tipo object

--- src/test/TestArreglosObject.java
+++ src/test/TestArreglosObject.java
@@ -16,5 +16,11 @@ public class TestArreglosObject {
         /*----------------------------------------------- Acceder al elemento ---------------------------------------------------------*/
         System.out.println("personas 0= " + personas[0]); //domain.Persona@Direccion_memoria (SIN TOSTRING)
         System.out.println("personas 1= " + personas[1]); 
+        
+        /*----------------------------------------------- Iterar el arreglo tipo object ------------------------------------------------*/
+        for (int i = 0; i < personas.length; i++) {
+            System.out.println("personas " + i + "= " + personas[i]); 
+        }
+        
     }
 }
