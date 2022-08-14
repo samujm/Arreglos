9f3cadc444feeaf2950b048cb55da2ceeff8fa52
Samantha González <samujm76@gmail.com>
2022-08-14 17:58:14 -0500

    ARREGLO DE TIPO PRIMITIVO

--- src/test/TestArreglos.java
+++ src/test/TestArreglos.java
@@ -7,7 +7,7 @@ public class TestArreglos {
     public static void main(String[] args) {
         //Definir un arreglo de tipo entero
         //NOTA: Los arreglos no pueden crecer, no se pueden agregar elementos de manera dinamica, para colecciones dinamicas son listas, set o map
-        /*------------------------------------------------- ARREGLO DE TIPO PRIMITIVO ---------------------------------------------------------------------------*/
+        /*---------------------------------------------------- ARREGLO DE TIPO PRIMITIVO -----------------------------------------------------------------------------*/
         int edades[] = new int[3]; //tipo nombre[] = new(YA QUE ES UN TIPO OBJECT) tipo_dato[Cantidad de elementos];
         System.out.println("edades = " + edades);//Imprime la dirección de memoria
         
